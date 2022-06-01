83c6faa644261fde2161e0cbeca1f537f7e4bb8b
Annise Arif <83833939+annise-arif@users.noreply.github.com>
2022-06-01 18:46:55 +0600

    my first next js

---
+++ pages/users/admin.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const admin = () => {
+    return (
+        <div>
+            <h1>This Is Admin Page</h1>
+        </div>
+    );
+};
+
+export default admin;
---
+++ pages/users/managers.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const managers = () => {
+    return (
+        <div>
+            <h1>All Managers are here</h1>
+        </div>
+    );
+};
+
+export default managers;
